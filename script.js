085a6cb7cf1ffe6ad01d45f5b7da8d487ad1854d
Maria S <mariz87@yandex.ru>
2025-10-28 13:13:27 +0300

    Add get by id hint text

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let secretNumber = Math.floor(Math.random() * 100) + 1;
 let guessInput = document.getElementById('guessInput');
 let checkButton = document.getElementById('checkBtn');
-console.log("Поле ввода:", guessInput);
-console.log("Кнопка:", checkButton);
+let hintText = document.getElementById('hintText');
+
+let secretNumber = Math.floor(Math.random() * 100) + 1;
+console.log("Загаданное число:", secretNumber);
