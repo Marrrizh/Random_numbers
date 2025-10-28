0552e75a35dba8da9134f802939b215a9cb8923d
Maria S <mariz87@yandex.ru>
2025-10-28 13:50:33 +0300

    Add function and checking button

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-let guessInput = document.getElementById('guessInput');
+let numberInput = document.getElementById('numberInput');
 let checkButton = document.getElementById('checkBtn');
 let hintText = document.getElementById('hintText');
 
 let secretNumber = Math.floor(Math.random() * 100) + 1;
 console.log("Загаданное число:", secretNumber);
+
+function checkNumber() {
+    let userGuess = Number(numberInput.value); 
+    
+    if (userGuess === secretNumber) {
+        hintText.textContent = "🎉 Угадала! Поздравляю!";
+    } else if (userGuess > secretNumber) {
+        hintText.textContent = "📉 Меньше!";
+    } else {
+        hintText.textContent = "📈 Больше!";
+    }
+}
+
+checkButton.addEventListener('click', checkNumber);
