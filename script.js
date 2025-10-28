c1197ba2018ebe2daeb0c1c229b9fc618546dd8f
Maria S <mariz87@yandex.ru>
2025-10-28 11:59:25 +0300

    Find Element by id

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 let secretNumber = Math.floor(Math.random() * 100) + 1;
-console.log("Загаданное число:", secretNumber);
+let guessInput = document.getElementById('guessInput');
+let checkButton = document.getElementById('checkBtn');
+console.log("Поле ввода:", guessInput);
+console.log("Кнопка:", checkButton);
