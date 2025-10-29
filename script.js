063ac5e64c99fef17062ce570689ab6234bc4ca8
Maria S <mariz87@yandex.ru>
2025-10-29 16:12:21 +0300

    add counter to js

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 let numberInput = document.getElementById('numberInput');
 let checkButton = document.getElementById('checkBtn');
 let hintText = document.getElementById('hintText');
+let attemptsDisplay=document.getElementById('attemptsCounter');
+
+let attempts=0;
+
 
 let secretNumber = Math.floor(Math.random() * 100) + 1;
 console.log("Загаданное число:", secretNumber);
 
 function checkNumber() {
     let userGuess = Number(numberInput.value); 
+    attempts = attempts + 1;
+  
     
     if (userGuess === secretNumber) {
         hintText.textContent = "🎉 Угадала! Поздравляю!";
+          attemptsDisplay.textContent = attempts;
     } else if (userGuess > secretNumber) {
         hintText.textContent = "📉 Меньше!";
+          attemptsDisplay.textContent = attempts;
     } else {
         hintText.textContent = "📈 Больше!";
+          attemptsDisplay.textContent = attempts;
     }
 }
 
